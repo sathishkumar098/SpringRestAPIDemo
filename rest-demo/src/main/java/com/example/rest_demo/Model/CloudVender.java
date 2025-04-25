61798a2cde012df79e2ce42c4517342cb7cd23ce
Sathish Kumar <sathishkumar.palanivel@davita.com>
2025-04-25 15:13:14 +0530

    Spring Rest API Demo

---
+++ rest-demo/src/main/java/com/example/rest_demo/Model/CloudVender.java
@@ -0,0 +1,50 @@
+package com.example.rest_demo.Model;
+
+public class CloudVender {
+    private String vendorId;
+    private String vendorName;
+    private String vendorAddress;
+    private String vendorPhoneNumber;
+
+    public CloudVender() {
+    }
+
+    public CloudVender(String vendorId, String vendorName, String vendorAddress, String vendorPhoneNumber) {
+        this.vendorId = vendorId;
+        this.vendorName = vendorName;
+        this.vendorAddress = vendorAddress;
+        this.vendorPhoneNumber = vendorPhoneNumber;
+    }
+
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(String vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorAddress() {
+        return vendorAddress;
+    }
+
+    public void setVendorAddress(String vendorAddress) {
+        this.vendorAddress = vendorAddress;
+    }
+
+    public String getVendorPhoneNumber() {
+        return vendorPhoneNumber;
+    }
+
+    public void setVendorPhoneNumber(String vendorPhoneNumber) {
+        this.vendorPhoneNumber = vendorPhoneNumber;
+    }
+}
